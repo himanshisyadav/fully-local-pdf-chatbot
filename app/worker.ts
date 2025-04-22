ec2a9b4648ae5b6fd817a8a7747bfe09ff1dd09e
himanshisyadav <himanshimj@gmail.com>
2025-04-22 15:52:57 -0500

    minor useless change

--- app/worker.ts
+++ app/worker.ts
@@ -56,19 +56,7 @@ Anything between the following \`context\` html blocks is retrieved from a knowl
 
 REMEMBER: If there is no relevant information within the context, just say "Hmm, I'm not sure." Don't try to make up an answer. Anything between the preceding 'context' html blocks is retrieved from a knowledge bank, not part of the conversation with the user.`;
 
-const WEBLLM_RESPONSE_SYSTEM_TEMPLATE = `You are an experienced researcher, expert at interpreting and answering questions based on provided sources. Using the provided context, answer the user's question to the best of your ability using the resources provided.
-Generate a concise answer for a given question based solely on the provided search results. You must only use information from the provided search results. Use an unbiased and journalistic tone. Combine search results together into a coherent answer. Do not repeat text, stay focused, and stop generating when you have answered the question.
-If there is nothing in the context relevant to the question at hand, just say "Hmm, I'm not sure." Don't try to make up an answer.`;
 
-const CHROME_AI_SYSTEM_TEMPLATE = `You are an AI assistant acting as an experienced researcher, expert at interpreting and answering questions based on provided sources. Using the provided context, answer the user's question to the best of your ability using the resources provided.
-Generate a concise answer for a given question based solely on the provided search results. You must only use information from the provided search results. Use an unbiased and journalistic tone. Combine search results together into a coherent answer. Do not repeat text, stay focused, and don't make up answers.
-
-When responding, use the following documents as context:\n<context>\n{context}\n</context>
-
-You do not need to exactly cite your sources from the above documents.
-
-{conversation_turns}
-assistant: `;
 
 const embedPDF = async (pdfBlob: Blob) => {
   const pdfLoader = new WebPDFLoader(pdfBlob, { parsedItemSeparator: " " });
